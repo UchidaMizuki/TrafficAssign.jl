8589d81b07daae5b5b1a30dcf4e5347e05cf3d83
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-07-07 01:16:38 +0900

    Update traffic.jl

--- src/traffic/traffic.jl
+++ src/traffic/traffic.jl
@@ -2,25 +2,25 @@ struct Traffic
     trips::SparseMatrixCSC{Float64, Int}
     graph::SimpleDiGraph{Int}
     link_performance::AbstractLinkPerformance
+end
 
-    function Traffic(tntp::TNTP)
-        n_nodes = tntp.n_nodes
+function Traffic(tntp::TNTP)
+    n_nodes = tntp.n_nodes
 
-        # trips
-        trips = tntp.trips
-        trips = sparse(trips.orig, trips.dest, trips.trips, n_nodes, n_nodes)
+    # trips
+    trips = filter(:trips => !=(0), tntp.trips)
+    trips = sparse(trips.orig, trips.dest, trips.trips, n_nodes, n_nodes)
 
-        # graph
-        network = tntp.network
-        graph = DiGraph(n_nodes)
+    # graph
+    network = tntp.network
+    graph = DiGraph(n_nodes)
 
-        for (from, to) in zip(network.from, network.to)
-            add_edge!(graph, from, to)
-        end
-
-        # link_performance
-        link_performance = BPR(tntp)
-
-        new(trips, graph, link_performance)
+    for (from, to) in zip(network.from, network.to)
+        add_edge!(graph, from, to)
     end
+
+    # link_performance
+    link_performance = BPR(tntp)
+
+    Traffic(trips, graph, link_performance)
 end
