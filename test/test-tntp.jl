d05613e8952515a40cb3b49272804f7890d99074
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-07-22 22:18:38 +0900

    Update test-tntp.jl

--- test/test-tntp.jl
+++ test/test-tntp.jl
@@ -1,10 +1,21 @@
 using TrafficAssign
 using Test
 
+using SparseArrays
+
+import Graphs
+
 @testset "tntp" begin
     tntp = load_tntp("SiouxFalls", toll_factor=1, length_factor=1)
 end
 
-tntp = load_tntp("SiouxFalls", toll_factor=1, length_factor=1)
+tntp = load_tntp("Anaheim", toll_factor=1, length_factor=1)
+traffic = Traffic(tntp)
 
-TrafficGraph(tntp)
+flow = all_or_nothing(traffic, 0.0)
+flow_new = all_or_nothing(traffic, flow)
+
+flow_new - flow
+
+a = sparse(1:3, 1:3, 1:3)
+b = sparse(1:3, [1, 2, 2], 1:3, 3, 3)
