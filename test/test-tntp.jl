4205e6b53dc9bbcadd5d73aa34c7507fac8370a1
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-08-05 00:00:56 +0900

    Update README

--- test/test-tntp.jl
+++ test/test-tntp.jl
@@ -5,15 +5,21 @@ using Test
     tntp = load_tntp("SiouxFalls")
 end
 
-# tntp = load_tntp("Anaheim")
-# # @time res = assign_traffic(tntp, max_iter=100, trace=true); nothing
-# @time res2 = assign_traffic(tntp, max_iter=100, algorithm=:frank_wolfe, trace=true); nothing
+tntp = load_tntp("Anaheim")
 
-# res.flow
-# res2.flow
-# res.objective < res2.objective
+@time res = assign_traffic(
+    tntp,
+    algorithm=FrankWolfe()
+)
+# @time res = assign_traffic(
+#     tntp,
+#     algorithm=ConjugateFrankWolfe()
+# )
+# @time res = assign_traffic(
+#     tntp,
+#     algorithm=BiconjugateFrankWolfe()
+# )
 
 # using TrafficAssignment
-# ta_data = load_ta_network("Anaheim")
-# @time link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, tol=1e-6, method=:fw, log=:on, max_iter_no=100); nothing
-# objective
+# ta_data = load_ta_network("GoldCoast")
+# @time link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, tol=1e-6, log=:on)
